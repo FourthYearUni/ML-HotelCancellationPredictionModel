c934c4ba27421a8da14c5731c351df7e3c977d41
Avici1 <alainchristian87@gmail.com>
2024-10-15 15:38:24 +0100

    added extra code in eda

--- cleaner.py
+++ cleaner.py
@@ -101,13 +101,6 @@ class Cleaner:
                 "YearMonth",
             ],
         )
-        # self.data_frame.drop("YearMonth")
-        # correlation_matrix = self.data_frame.corr()
-        # relevant_f = correlation_matrix["reservation_status_Canceled"].sort_values(
-        #   ascending=False
-        # )
-
-        # relevant_f.to_excel("correlation_matrix.xlsx")
 
         # print(relevant_f)
 
---
+++ feature_engineering.py
@@ -0,0 +1,7 @@
+# self.data_frame.drop("YearMonth")
+# correlation_matrix = self.data_frame.corr()
+# relevant_f = correlation_matrix["reservation_status_Canceled"].sort_values(
+#   ascending=False
+# )
+
+# relevant_f.to_excel("correlation_matrix.xlsx")
